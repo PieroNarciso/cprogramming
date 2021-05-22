16a7aca411696b1a6313c243117183ef149a0167
Piero Narciso <pieronarciso988@gmail.com>
2021-05-21 21:38:56 -0500

    Climbin Stairs
    * DP

--- LeetCode/70.climbing_stairs.cpp
+++ LeetCode/70.climbing_stairs.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
+int climbRecursive(int n, unordered_map<int, int> &memo) {
+	if (n == 1) {
+		return 1;
+	}
+	if (n == 2) {
+		return 2;
+	}
+	if (memo.find(n) != memo.end()) {
+		return memo[n];
+	}
+	memo[n] = climbRecursive(n-1, memo) + climbRecursive(n-2, memo);
+	return memo[n];
+}
+
 int climbStairs(int n) {
-	// TODO
-	return 0;
+	unordered_map<int, int> memo;
+	return climbRecursive(n, memo);
+}
+
+
+int main(int argc, const char** argv) {
+	cout << climbStairs(44) << endl;
+    return 0;
 }
